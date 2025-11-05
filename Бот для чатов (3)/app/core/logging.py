1f3848be6c1c95e2b2210693b72e1e7306b262cd
Drynya228 <55211357+Drynya228@users.noreply.github.com>
2025-11-05 19:12:24 +0300

    Delete Бот для чатов (3) directory

--- Бот для чатов (3)/app/core/logging.py
+++
@@ -1,2 +0,0 @@
-from loguru import logger
-logger.add(lambda msg: print(msg, end=""))
--- Бот для чатов (3)/app/workers/queue.py
+++
@@ -1,6 +0,0 @@
-from rq import Queue
-from redis import Redis
-from app.core.config import cfg
-
-redis = Redis.from_url(cfg.REDIS_URL)
-queue = Queue("default", connection=redis)
